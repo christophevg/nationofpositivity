ecd52dff7204e0a46cbfe0ffdf070fe07d609f6a
Christophe VG <contact@christophe.vg>
2024-10-26 17:57:35 +0200

    don't show drawer by default when browser is small

--- shop/app.js
+++ shop/app.js
@@ -1,2 +1,4 @@
-// don't show drawer by (baseweb) default
-before_app_mount( function() { store.commit("toggle_drawer"); } );
+// don't show drawer by (baseweb) default when screen is small
+if($(document).width() < 1200 ) {
+  before_app_mount( function() { store.commit("toggle_drawer"); } );
+}
