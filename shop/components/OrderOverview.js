30418beb90f4961c18fed3f118f9598bef4f5cdb
Christophe VG <contact@christophe.vg>
2024-10-28 12:58:47 +0100

    created XS order overview

--- shop/components/OrderOverview.js
+++ shop/components/OrderOverview.js
@@ -1,7 +1,112 @@
 Vue.component("OrderOverview", {
   props: {
-    "order"       : Object,
-    "editable"    : Boolean,
+    "order"      : Object,
+    "editable"   : Boolean,
+    "withExtras" : Boolean
+  },
+  template: `
+<div>
+  <OrderOverviewAsDataTable class="hidden-xs-only"   :order="order" :editable="editable" :with-extras="withExtras" @on_add="add" @on_remove="remove"/>
+  <OrderOverviewAsList      class="hidden-sm-and-up" :order="order" :editable="editable" :with-extras="withExtras" @on_add="add" @on_remove="remove"/>
+</div>
+`,
+  methods: {
+    add: function(item) {
+      this.$emit("on_add", item);
+    },
+    remove: function(item) {
+      this.$emit("on_remove", item);
+    }
+  }
+});
+
+Vue.component("OrderOverviewAsList", {
+  props: {
+    "order"      : Object,
+    "editable"   : Boolean,
+    "withExtras" : Boolean
+  },
+  template: `
+<v-card>
+  <v-list two-line subheader>
+    <v-subheader>Producten</v-subheader>
+
+    <v-list-tile v-for="(item, i) in order.lines" :key="i">
+      <v-list-tile-content>
+
+        <v-list-tile-title>
+          <router-link :to="'/products/' + item.product.id">{{ item.product.title }}</router-link>
+        </v-list-tile-title>
+
+        <v-list-tile-sub-title>
+          {{ summary(item.options )}}<br>
+          &euro; {{ item.unit_price | fixed2 }} x {{ item.amount }} = &euro; {{ item.line_total | fixed2  }}
+        </v-list-tile-sub-title>
+
+      </v-list-tile-content>
+
+        <v-list-tile-action>
+          <div>
+          <v-icon v-if="editable" small class="mr-2" @click="remove(item)">remove</v-icon>
+          <v-icon v-if="editable" small class="mr-2" @click="add(item)">add</v-icon>
+          </div>
+        </v-list-tile-action>
+
+    </v-list-tile>
+
+  </v-list>
+
+  <v-divider></v-divider>
+          
+  <div style="margin-bottom: 20px">
+    <v-subheader>Totaal</v-subheader>
+    
+    <table width="100%" cellspacing=0 cellpadding="0" border="0">
+      <tr>
+        <th class="text-xs-right">Artikels</th><td class="text-xs-right">&euro;{{ order.total.lines | fixed2 }}</td>
+      </tr>
+      <tr class="text-xs-right" v-if="withExtras">
+        <th>Verzending</th><td class="text-xs-right">&euro; {{ order.total.shipping | fixed2 }}</td>
+      </tr>
+      <tr class="text-xs-right" v-if="withExtras && order.total.payment > 0">
+        <th>Online betalen</th><td class="text-xs-right">&euro; {{ order.total.payment | fixed2 }}</td>
+      </tr>
+      <tr class="text-xs-right" v-if="withExtras">
+        <th>Totaal</th><td class="text-xs-right">&euro; {{ order.total.grand | fixed2 }}</td>
+      </tr>
+      <tr class="text-xs-right" v-if="withExtras">
+        <th>Waarvan 21% BTW</th><td class="text-xs-right">&euro; {{ order.total.tax | fixed2 }}</td>
+      </tr>
+    </table>
+  </div>
+
+</v-card>
+`,
+  computed: {
+    summary: function() {
+      return function(options) {
+        return options.map(function(option) {
+          return option.option + " : " + option.choice.substring(0,30) + ( option.choice.length > 30 ? "..." : "") + (
+            option.cost > 0 ? " (+€" + option.cost + ")" : "" 
+          );
+        }).join(", ");
+      }
+    }
+  },
+  methods: {
+    add: function(item) {
+      this.$emit("on_add", item);
+    },
+    remove: function(item) {
+      this.$emit("on_remove", item);
+    }
+  }
+});
+
+Vue.component("OrderOverviewAsDataTable", {
+  props: {
+    "order"      : Object,
+    "editable"   : Boolean,
     "withExtras" : Boolean
   },
   template: `
